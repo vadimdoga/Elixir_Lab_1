de858be7fe89f8e971474da8291548246cc02f35
vadimdoga <vadim.doga1@gmail.com>
2020-03-12 10:43:03 +0200

    refactored to GenServer

--- lib/fetch_actor.ex
+++ lib/fetch_actor.ex
@@ -1,12 +1,22 @@
 require Root
 
 defmodule Fetch do
-  def start_link(url) do
+  use GenServer
+
+  def start_link(_url) do
+    # IO.inspect("starting", label: "Counter.One")
+    GenServer.start_link(__MODULE__, "http://localhost:4000/iot")
+    # IO.inspect("started", label: "Counter.One")
+  end
+
+  def init(url) do
     {:ok, _pid} = EventsourceEx.new(url, stream_to: self())
     root_pid = spawn_link(Root, :recv, [])
     :ets.new(:buckets_registry, [:named_table])
     :ets.insert(:buckets_registry, {"root_pid", root_pid})
+
     recv()
+    {:ok, url}
   end
 
   def recv do
@@ -19,12 +29,8 @@ defmodule Fetch do
 
   def msg_operations(msg) do
     data = json_parse(msg)
-    cond do
-      data == :panic_msg -> recv()
-      true -> [{_id, root}] = :ets.lookup(:buckets_registry, "root_pid")
-      send(root, {:data, data})
-    end
-
+    [{_id, root}] = :ets.lookup(:buckets_registry, "root_pid")
+    send(root, {:data, data})
   end
 
   def json_parse(msg) do
@@ -32,16 +38,7 @@ defmodule Fetch do
       msg_data = Jason.decode!(msg.data)
       msg_data["message"]
     rescue
-      Jason.DecodeError -> :panic_msg
+      Jason.DecodeError -> raise "panic_msg"
     end
-  end
-
-  def child_spec(opts) do
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, [opts]},
-      type: :worker,
-      restart: :permanent,
-    }
   end
 end
--- lib/lab1/application.ex
+++ lib/lab1/application.ex
@@ -3,10 +3,9 @@ defmodule Lab1.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec
 
     children = [
-      supervisor(Supervisor_1, ["http://localhost:4000/iot"])
+      Fetch
     ]
 
     opts = [strategy: :one_for_one, name: Lab1.Supervisor]
