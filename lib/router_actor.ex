df9af5491d5edf5ce7f2f7a1adb78dee932a0340
vadimdoga <vadim.doga1@gmail.com>
2020-03-19 17:00:59 +0200

    refactored code

--- lib/router_actor.ex
+++ lib/router_actor.ex
@@ -10,32 +10,41 @@ defmodule Router do
   def msg_operations(msg, list_pid) do
     list_pid_size = Enum.count(list_pid)
     #generate a random name
-    name =
-      ?a..?z
-      |> Enum.take_random(6)
-      |> List.to_string()
+    name = generate_name()
     #if list_pid is less then 20 add a new actor
+    compare_add(list_pid, list_pid_size, name, msg)
+    #if list_pid is more then 20 it will go through pid_list and remove each pid using supervisor
+    compare_remove(list_pid, list_pid_size)
+  end
+
+  defp compare_add(list_pid, list_pid_size, name, msg) do
     if list_pid_size < 20 do
       {state, pid} = DynSupervisor.add_slave(name, msg)
       if state == :error do
         recv(list_pid)
       end
       list_pid = list_pid ++ [pid]
-      IO.inspect(pid)
-      IO.inspect(DynSupervisor.count_children())
+      # IO.inspect(pid)
+      # IO.inspect(DynSupervisor.count_children())
       recv(list_pid)
     end
-    #if list_pid is more then 20 it will go through pid_list and remove each pid using supervisor
+  end
+
+  defp compare_remove(list_pid, list_pid_size) do
     if list_pid_size >= 20 do
       Enum.map(0..19, fn x ->
         pid = Enum.at(list_pid, x)
-        IO.inspect(pid)
-        pid = DynSupervisor.rm_slave(pid)
-        IO.inspect(pid)
+        # IO.inspect(pid)
+        _pid = DynSupervisor.rm_slave(pid)
+        # IO.inspect(pid)
       end)
       list_pid = []
       recv(list_pid)
     end
+  end
+
+  defp generate_name do
+    ?a..?z|> Enum.take_random(6)|> List.to_string()
   end
 
   def child_spec(opts) do
